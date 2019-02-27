7e160785696de56067d132f396533f9273bb5497
Thomas Müller <thomas.mueller@tmit.eu>
2019-02-27 17:49:05 +0100

    Apply UiKit to checkbox

--- src/elements/OcCheckbox.vue
+++ src/elements/OcCheckbox.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <label class="container">
-      <input type="checkbox" v-model="model" @change="onChange" />
-      <span class="checkmark"></span>
-    </label>
-  </div>
+  <input type="checkbox" v-model="model" @change="onChange" class="uk-checkbox" />
 </template>
 <script>
 export default {
@@ -34,62 +29,7 @@ export default {
   },
 }
 </script>
-<style lang="scss" scoped>
-.container {
-  display: inline-block;
-  position: relative;
-  margin-bottom: 12px;
-  cursor: pointer;
-  font-size: 22px;
-  user-select: none;
-  & input {
-    position: relative;
-    opacity: 0;
-    cursor: pointer;
-    height: 0;
-    width: 0;
-    &:checked ~ .checkmark {
-      background-color: $color_oc_blue;
-    }
-    &:checked ~ .checkmark:after {
-      display: block;
-    }
-  }
-  &:hover input ~ .checkmark {
-    background-color: $color_oc_blue;
-    &:after {
-      position: absolute;
-      left: 9px;
-      top: 5px;
-      width: 5px;
-      height: 10px;
-      border: solid white;
-      border-width: 0 3px 3px 0;
-      transform: rotate(45deg);
-    }
-  }
-}
-
-.checkmark {
-  position: relative;
-  float: right;
-  height: 25px;
-  width: 25px;
-  background-color: #eee;
-  &:after {
-    content: "";
-    position: absolute;
-    display: none;
-    left: 9px;
-    top: 5px;
-    width: 5px;
-    height: 10px;
-    border: solid white;
-    border-width: 0 3px 3px 0;
-    transform: rotate(45deg);
-  }
-}
-</style>
+<style lang="scss" scoped></style>
 <docs>
   ```jsx
   <div>
