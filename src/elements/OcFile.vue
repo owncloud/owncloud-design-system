471904cccc716da135796a52e7c693c52925cf41
Felix Heidecke <felix@heidecke.me>
2019-03-19 20:19:56 +0100

    Calculate name and extension inside ocFile

--- src/elements/OcFile.vue
+++ src/elements/OcFile.vue
@@ -1,8 +1,11 @@
 <template>
   <a class="oc-file">
     <oc-icon :name="icon" variation="primary" class="uk-position-center-left" />
-    <span class="oc-file-name">{{ name }}</span
-    ><span v-if="extension" class="oc-file-extension">{{ extension }}</span>
+    <span class="oc-file-name" v-text="_name" /><span
+      v-if="file.extension"
+      class="oc-file-extension"
+      v-text="file.extension"
+    />
   </a>
 </template>
 <script>
@@ -23,27 +26,39 @@ export default {
       default: "folder",
     },
     /**
-     * The extension of the file/folder to be displayed.
+     * A file object with necessary property `name` and an optional property `extension`
      */
-    extension: {
-      type: String,
-      required: false,
-    },
-    /**
-     * The name of the file/folder to be displayed.
-     */
-    name: {
-      type: String,
+    file: {
+      type: Object,
       required: true,
+    },
+  },
+  computed: {
+    _name() {
+      let name = this.file.name,
+        extension = this.file.extension
+
+      if (!extension) {
+        return name
+      }
+
+      return name.substring(0, name.length - extension.length - 1)
     },
   },
 }
 </script>
 <docs>
-    ```jsx
-    <div>
-        <oc-file name="This is a picture"/>
-        <oc-file icon="image" extension="png" name="picture"/>
-    </div>
-    ```
+```jsx
+<ul class="uk-list">
+  <li>
+    <oc-file icon="image" :file="{ name : 'I love flowers.jpg', extension : 'jpg' }" />
+  </li>
+  <li>
+    <oc-file icon="folder" :file="{ name : 'Large folder' }" />
+  </li>
+  <li>
+    <oc-file icon="image" :file="{ name : 'Just kidding, I hate flowers.png', extension : 'png' }" />
+  </li>
+</ul>
+```
 </docs>
