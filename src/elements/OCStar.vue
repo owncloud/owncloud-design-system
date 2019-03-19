2698826dd541458a050f529c5f08e712fe57ea8f
Felix Heidecke <felix@heidecke.me>
2019-03-19 20:17:59 +0100

    Add ocStar element

---
+++ src/elements/OCStar.vue
@@ -0,0 +1,33 @@
+<template>
+  <oc-icon class="oc-star" :name="_name" />
+</template>
+<script>
+export default {
+  name: "oc-star",
+  status: "review",
+  release: "1.0.0",
+  props: {
+    /**
+     * Image source, path to avatar
+     **/
+    shining: {
+      type: Boolean,
+      required: false,
+      defailt: true,
+    },
+  },
+  computed: {
+    _name() {
+      return this.shining ? "star" : "star_border"
+    },
+  },
+}
+</script>
+<docs>
+```jsx
+<div>
+  <oc-star/>
+  <oc-star shining/>
+</div>
+```
+</docs>
