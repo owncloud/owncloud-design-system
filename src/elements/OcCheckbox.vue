b763bf0c3c9320cfb5ca0f3b45bbecf5b15465cd
Thomas Müller <thomas.mueller@tmit.eu>
2019-03-21 10:16:29 +0100

    oc-checkbox: fix documentation and property handling

--- src/elements/OcCheckbox.vue
+++ src/elements/OcCheckbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <input class="uk-checkbox ff" type="checkbox" :checked="checked" v-model="model" />
+  <input class="uk-checkbox" type="checkbox" v-model="checkboxState" />
 </template>
 <script>
 /**
@@ -9,13 +9,46 @@ export default {
   name: "oc-checkbox",
   status: "review",
   release: "1.0.0",
-  props: ["model", "checked"],
+  props: {
+    /**
+     * Data-model
+     *
+     * @model
+     **/
+    model: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    checkboxState: {
+      get: function() {
+        return this.model
+      },
+      set: function(value) {
+        /**
+         * State change event
+         * @event stateChanged
+         * @type {event}
+         */
+        this.$emit("stateChanged", value)
+      },
+    },
+  },
 }
 </script>
 <docs>
-```jsx
-<div>
-  <oc-checkbox />
-</div>
-```
+  ```jsx
+  <div>
+    <section>
+      <h3 class="uk-heading-divider">
+        Checkboxes Types
+      </h3>
+      <oc-checkbox />
+      <oc-checkbox :model="true"/>
+    </section>
+
+  </div>
+  ```
 </docs>
