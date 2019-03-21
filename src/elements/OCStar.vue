91b395469c5a09f0ceac879c46eee63426457898
Thomas Müller <thomas.mueller@tmit.eu>
2019-03-21 08:55:16 +0100

    oc-star: Fix documentation and typos

--- src/elements/OCStar.vue
+++ src/elements/OCStar.vue
@@ -2,18 +2,23 @@
   <oc-icon class="oc-star" :name="_name" />
 </template>
 <script>
+/**
+ * A star is used to mark a favorite element.
+ *
+ * It has basically two states: normal and shining.
+ */
 export default {
   name: "oc-star",
   status: "review",
   release: "1.0.0",
   props: {
     /**
-     * Image source, path to avatar
+     * Make the star shine ....
      **/
     shining: {
       type: Boolean,
       required: false,
-      defailt: true,
+      default: false,
     },
   },
   computed: {
@@ -26,8 +31,16 @@ export default {
 <docs>
 ```jsx
 <div>
-  <oc-star/>
-  <oc-star shining/>
+  <section>
+    <h3 class="uk-heading-divider">
+      Just a star
+    </h3>
+    <oc-star/>
+    <h3 class="uk-heading-divider">
+      A shining star
+    </h3>
+    <oc-star shining/>
+  </section>
 </div>
 ```
 </docs>
