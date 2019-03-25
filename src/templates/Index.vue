dea053d5c9a1f6c3c724f1a83b0c9bf5031e9dcc
Thomas Müller <thomas.mueller@tmit.eu>
2019-03-25 21:25:04 +0100

    We need at least one template ....

---
+++ src/templates/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="type" class="index"> </component>
+</template>
+
+<script>
+/**
+ * Shows how to layout and structure a home page.
+ */
+export default {
+  name: "Index",
+  status: "deprecated",
+  release: "1.0.0",
+  metaInfo: {
+    title: "ownCloud Design System",
+    htmlAttrs: {
+      lang: "en",
+    },
+  },
+  props: {
+    /**
+     * The html element name used for the component.
+     */
+    type: {
+      type: String,
+      default: "div",
+    },
+  },
+}
+</script>
+
+<docs>
+  ```jsx
+  <Index />
+  ```
+</docs>
