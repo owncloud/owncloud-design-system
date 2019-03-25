269fa4ed0ca03c73572c3ab34ee201a37c42c6cc
Felix Heidecke <felix@heidecke.me>
2019-03-25 13:35:10 +0100

    First draft of autocomplete function

--- src/patterns/OcAutocomplete.vue
+++ src/patterns/OcAutocomplete.vue
@@ -1,19 +1,47 @@
 <template>
-  <div>
-    <oc-text-input :disabled="loading" :placeholder="label" @change="userInput"></oc-text-input>
-    <oc-spinner v-if="loading"></oc-spinner>
+  <div class="oc-autocomplete">
+    <!-- <oc-text-input :disabled="loading" :placeholder="label" @change="userInput"></oc-text-input> -->
+    <input class="uk-input" v-model="input" @keydown="key($event)" :placeholder="label" />
+    <div hidden :id="_boundry" />
+    <div
+      class="uk-card uk-card-default uk-card-body uk-padding-remove"
+      :uk-drop="'delay-hide:0;toggle:#' + _boundry"
+      :id="_dropDown"
+    >
+      <ul class="uk-list">
+        <template v-for="(item, i) in matches">
+          <li
+            :class="[
+              { 'oc-autocomplete-suggestion-selected': i === highlighted },
+              'oc-autocomplete-suggestion',
+            ]"
+            :key="i"
+            v-text="item"
+            @mouseenter="highlighted = i"
+            @click="selectSuggestion"
+          ></li>
+        </template>
+      </ul>
+    </div>
   </div>
 </template>
-
 <script>
 import OcSpinner from "../elements/OcSpinner"
 import OcTextInput from "../elements/OcTextInput"
+
+import UiKit from "uikit"
+import { uniqueId as _uniqueId } from "lodash"
 
 /**
  * The autocomplete component is used for searching in bigger list of data (usually involves remote calls over the network)
  * and presenting the user a list of possible matches to the entered search term.
  *
  * The component supports single select only at the moment.
+ *
+ * ##TODO:
+ *
+ * - [ ] Implement loading indicator
+ * - [ ] Allow complex content (HTML)
  */
 export default {
   name: "oc-autocomplete",
@@ -27,6 +55,7 @@ export default {
     label: {
       type: String,
       required: true,
+      default: null,
     },
     /**
      * Is set to true data is loaded and the user cannot enter further data
@@ -51,6 +80,58 @@ export default {
       type: String,
     },
   },
+  data() {
+    return {
+      input: "",
+      highlighted: 0,
+    }
+  },
+  computed: {
+    matches() {
+      if (this.input.length === 0) {
+        return []
+      }
+
+      return this.items.filter(item => {
+        return item.toLowerCase().indexOf(this.input.toLowerCase()) >= 0
+      })
+    },
+    _dropDown() {
+      return _uniqueId("oc-autocomplete-dropdown-")
+    },
+    _boundry() {
+      return _uniqueId("oc-autocomplete-boundry-")
+    },
+    _dropdown() {
+      return UiKit.drop(`#${this._dropDown}`)
+    },
+  },
+  watch: {
+    matches(items, last) {
+      if (items === last) return
+
+      // Hide if the list only contains the current full match
+      if (items.length === 1 && items[0].toLowerCase() === this.input.toLowerCase()) {
+        this._dropdown.hide()
+      }
+      // Hide, if there are no matches
+      else if (items.length === 0) {
+        this._dropdown.hide()
+      } else {
+        this._dropdown.show()
+      }
+    },
+    highlighted(next, current) {
+      if (next === current) return
+
+      // come around
+      if (next < 0) {
+        this.highlighted = this.matches.length - 1
+      } else if (next > this.matches.length - 1) {
+        this.highlighted = 0
+      }
+    },
+  },
   methods: {
     userInput(value) {
       /**
@@ -58,19 +139,28 @@ export default {
        */
       this.$emit("update:search-input", value)
     },
+    key(event) {
+      switch (event.key) {
+        case "ArrowUp":
+          this.highlighted--
+          break
+        case "ArrowDown":
+          this.highlighted++
+          break
+        case "Enter":
+          this.selectSuggestion()
+      }
+    },
+    selectSuggestion() {
+      if (this.matches[this.highlighted]) this.input = this.matches[this.highlighted]
+    },
   },
 }
 </script>
-
-<style scoped></style>
-
 <docs>
-    ```jsx
-    <div>
-        <oc-autocomplete :loading="true" label="Add user or group ..."></oc-autocomplete>
-        <oc-autocomplete :items="['Alice', 'Bob', 'Chuck']"
-                         label="Add user or group ..."
-        />
-    </div>
-    ```
+```jsx
+<div>
+  <oc-autocomplete :items="['Alice', 'Bob', 'Chuck', 'Doris', 'Emil', 'Felix']" label="Add user or group ..." />
+</div>
+```
 </docs>
