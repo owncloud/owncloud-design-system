06b64f673c3eb34e8413e2c73ab3bba59118f407
Thomas Müller <thomas.mueller@tmit.eu>
2019-03-25 12:44:32 +0100

    New child elemets for top bar: oc-topbar-logo and oc-topbar-item

---
+++ src/patterns/_OcTopBarItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="uk-navbar-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "oc-topbar-item",
+  status: "review",
+  release: "1.0.0",
+}
+</script>
---
+++ src/patterns/_OcTopBarLogo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="uk-navbar-item uk-logo">
+    <oc-icon :name="icon"></oc-icon>
+    <span v-if="title" class="uk-navbar-item">{{ title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "oc-topbar-logo",
+  status: "review",
+  release: "1.0.0",
+  props: {
+    icon: {
+      type: String,
+      default: "menu",
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
