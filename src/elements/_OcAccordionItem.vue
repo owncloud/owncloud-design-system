188ae294204320ae61bd0251aa172d065244ae6f
Tom Needham <tom@owncloud.com>
2019-03-26 14:00:17 +0000

    Add accordion item element and accordion pattern

---
+++ src/elements/_OcAccordionItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <li>
+    <a class="uk-accordion-title" href="#">
+      {{ title }}
+      <span v-if="subtitle !== undefined" class="uk-text-meta uk-margin-left">{{ subtitle }}</span>
+    </a>
+    <div class="uk-accordion-content">
+      <slot name="content">Content of them item is here</slot>
+    </div>
+  </li>
+</template>
+<script>
+export default {
+  name: "oc-accordion-item",
+  status: "review",
+  release: "1.0.0",
+  props: {
+    /**
+     * The title of the item shown in the list
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Subtitle shown after the main title, in the accordion list
+     */
+    subtitle: {
+      type: String,
+      required: false,
+    },
+  },
+}
+</script>
+<docs>
+```jsx
+<oc-accordion>
+  <oc-accordion-item title="Public link share">
+    <template slot="content">
+      <p>Hey, I can be expanded</p>
+    </template>
+  </oc-accordion-item>
+</oc-accordion>
+```
+</docs>
---
+++ src/patterns/OcAccordion.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul :uk-accordion="accordionOptions">
+    <!-- @slot Many oc-accordion-item elements  -->
+    <slot />
+  </ul>
+</template>
+<script>
+/**
+ * Creates a list with items that are expandable
+ */
+export default {
+  name: "oc-accordion",
+  status: "review",
+  release: "1.0.0",
+  props: {
+    /**
+     * Allow multiple items to be expanded at the same time
+     */
+    multiple: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  computed: {
+    accordionOptions() {
+      return "multiple: " + this.multiple
+    },
+  },
+}
+</script>
+<docs>
+```jsx
+<oc-accordion :multiple=false>
+  <oc-accordion-item title="My accordion item"></oc-accordion-item>
+  <oc-accordion-item title="Something else with content" subtitle="And a subtitle">
+    <template slot="content">
+      <p>Enter some text!</p>
+      <oc-text-input></oc-text-input>
+    </template>
+  </oc-accordion-item>
+</oc-accordion>
+```
+</docs>
