cbd41834838a502f9f8150ed6349efc48a56a0b8
Jörn Friedrich Dreyer <jfd@butonic.de>
2019-03-26 11:47:32 +0100

    add tabs and tab-item
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

---
+++ src/elements/OcTabItem.vue
@@ -0,0 +1,75 @@
+<template>
+  <li :class="_itemClass" @click="onClick">
+    <a href="" v-text="title"></a>
+  </li>
+</template>
+<script>
+/**
+ * Create a tab item.
+ */
+export default {
+  name: "oc-tab-item",
+  status: "prototype",
+  release: "1.0.0",
+  props: {
+    /**
+     * The title to display.
+     */
+    title: {
+      type: String,
+      required: true,
+    },
+    /**
+     * Display as active.
+     */
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * Disable the tab.
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    _itemClass() {
+      return this.disabled ? "uk-disabled" : this.active ? "uk-active" : ""
+    },
+  },
+  methods: {
+    onClick(val) {
+      /**
+       * Click event
+       * @event click
+       * @type {event}
+       */
+      this.$emit("click", val)
+    },
+  },
+}
+</script>
+
+<docs>
+```jsx
+  <template>
+    <oc-tabs>
+      <oc-tab-item active title="Left" @click="onClick" />
+      <oc-tab-item title="Item 1"/>
+      <oc-tab-item title="Item 2"/>
+      <oc-tab-item disabled title="Disabled"/>
+    </oc-tabs>
+  </template>
+  <script>
+    export default {
+      methods: {
+        onClick(val) {
+          alert(val)
+        }
+      }
+    }
+  </script>
+```
+</docs>
---
+++ src/patterns/OcTabs.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul uk-tab>
+    <slot />
+  </ul>
+</template>
+
+<script>
+import OcTabItem from "../elements/OcTabItem"
+/**
+ * Create a tabbed navigation.
+ *
+ * ##TODO:
+ *
+ * - [ ] Implement https://getuikit.com/docs/tab#bottom-modifier
+ * - [ ] Implement https://getuikit.com/docs/tab#left-right-modifiers
+ * - [ ] Implement https://getuikit.com/docs/tab#alignment
+ * - [ ] Implement https://getuikit.com/docs/tab#tabs-and-dropdown
+ * - [ ] Implement https://getuikit.com/docs/tab#component-options
+ */
+export default {
+  name: "oc-tabs",
+  components: { OcTabItem },
+  status: "prototype",
+  release: "1.0.0",
+}
+</script>
+
+<docs>
+```jsx
+  <template>
+    <oc-tabs>
+      <oc-tab-item active title="Left" @click="onClick" />
+      <oc-tab-item title="Item 1"/>
+      <oc-tab-item title="Item 2"/>
+      <oc-tab-item disabled title="Disabled"/>
+    </oc-tabs>
+  </template>
+  <script>
+    export default {
+      methods: {
+        onClick(val) {
+          alert(val)
+        }
+      }
+    }
+  </script>
+```
+</docs>
