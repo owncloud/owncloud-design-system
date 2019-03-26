e27c5e4641cd999bae436e1af5bd5f55ffe38bf8
Felix Heidecke <felix@heidecke.me>
2019-03-26 12:01:09 +0100

    Add OcLoader Add Loader to Loading Demo

---
+++ src/templates/Loader-to-Loading.vue
@@ -0,0 +1,43 @@
+<docs>
+```jsx
+    <template>
+        <div>
+            <p>
+                {{info}}
+            </p>
+            <oc-progress v-if="state >= 0" :value="state" :max="max" />
+            <oc-loader v-else />
+        </div>
+    </template>
+    <script>
+    export default {
+        data () {
+            return {
+                state : -1,
+                max : 10
+            }
+        },
+        methods : {
+            load() {
+                let step = setInterval( () => {
+                    if (this.state === this.max)
+                        clearInterval(step)
+
+                    this.state++
+                }, 1000)
+            }
+        },
+        mounted () {
+            setTimeout(() => {
+                this.load()
+            }, 4500)
+        },
+        computed : {
+            info () {
+                return (this.state >= 0) ? 'Loading' : 'Loader (indeterminate)'
+            }
+        }
+    }
+    </script>
+```
+</docs>
