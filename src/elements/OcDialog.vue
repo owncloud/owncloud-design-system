96499ea4681c5312a55494a19ab351b2b5ab60d6
Thomas Müller <thomas.mueller@tmit.eu>
2019-03-26 16:27:44 +0100

    oc-dialog: visibility of the dialog is bound to v-model

--- src/elements/OcDialog.vue
+++ src/elements/OcDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="name" uk-modal>
+  <div :id="name" ref="dialog" uk-modal>
     <div class="uk-modal-dialog uk-modal-body">
       <h2 class="uk-modal-title">{{ title }}</h2>
       <slot name="content">
@@ -9,11 +9,21 @@
   </div>
 </template>
 <script>
+import UIkit from "uikit"
+
 export default {
   name: "oc-dialog",
   status: "prototype",
   release: "1.0.0",
   props: {
+    /**
+     * Data-model
+     **/
+    value: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
     /**
      * Id or Name of the dialog. Used to reference for open.
      *
@@ -31,20 +41,47 @@ export default {
       required: false,
     },
   },
+  watch: {
+    value: function(valNew) {
+      if (valNew) {
+        UIkit.modal("#" + this.name).show()
+      } else {
+        UIkit.modal("#" + this.name).hide()
+      }
+    },
+  },
 }
 </script>
 <docs>
   ```jsx
-  <button uk-toggle="target: #demoModal" type="button">Open</button>
-  <oc-dialog name="demoModal" title="Demotitle" />
-
-  <button uk-toggle="target: #contentExample" type="button">Content Slot</button>
-  <oc-dialog name="contentExample" title="Create Folder">
-    <template slot="content">
-      <oc-text-input placeholder="New Folder Name"/>
-      <oc-button class="uk-modal-close" text="Create" variation="primary" type="button"></oc-button>
-      <oc-button class="uk-modal-close" text="Cancel" variation="secondary" type="button"></oc-button>
-    </template>
-  </oc-dialog>
+  <template>
+    <section>
+      <h3 class="uk-heading-divider">
+        Prompt Dialog
+      </h3>
+      <oc-button @click="showDialog" text="Open"></oc-button>
+      <oc-dialog name="demoModal" title="Create Folder" v-model="dialogModel">
+        <template slot="content">
+          <oc-text-input placeholder="New Folder Name"/>
+          <oc-button class="uk-modal-close" text="Create" variation="primary" type="button"></oc-button>
+          <oc-button class="uk-modal-close" text="Cancel" variation="secondary" type="button"></oc-button>
+        </template>
+      </oc-dialog>
+    </section>
+  </template>
+  <script>
+    export default {
+      data: () => {
+        return {
+          dialogModel: false
+        }
+      },
+      methods: {
+        showDialog() {
+          this.dialogModel = true
+        }
+      }
+    }
+  </script>
   ```
 </docs>
