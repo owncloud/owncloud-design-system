6fde78e6bd59e8c89a55df0a7353d5cd7467be13
Felix Heidecke <felix@heidecke.me>
2019-04-16 15:00:09 +0200

    Introduce OCDrop Element

---
+++ src/elements/OCDrop.vue
@@ -0,0 +1,117 @@
+<template>
+  <div :uk-drop="$_ocDrop_props">
+    <div v-if="$slots.default" class="uk-card uk-card-default uk-card-small uk-card-body">
+      <slot />
+    </div>
+    <slot v-else name="special" />
+  </div>
+</template>
+<script>
+/**
+ * Position any element in relation to another element.
+ */
+export default {
+  name: "oc-drop",
+  status: "review",
+  release: "1.0.0",
+  props: {
+    /**
+     * CSS selector of the element to maintain the drop's visibility. see https://getuikit.com/docs/drop#boundary
+     **/
+    boundary: {
+      type: [String, Boolean],
+      default: false,
+    },
+    /**
+     * CSS selector for the element to be used as toggle. By default, the preceding element is used.
+     **/
+    toggle: {
+      type: String,
+      default: "- *",
+    },
+    /**
+     * Comma separated list of drop trigger behaviour modes: `click or hover`
+     **/
+    mode: {
+      type: String,
+      default: "click",
+      validator: value => {
+        return value.match(/(click|hover)/)
+      },
+    },
+    /**
+     * More options are available - see https://getuikit.com/docs/drop
+     **/
+    options: {
+      type: Object,
+      default() {
+        return {}
+      },
+    },
+  },
+  computed: {
+    $_ocDrop_props() {
+      // from properties
+      let boundary = this.boundary || this.toggle
+      let toggle = this.toggle
+      let mode = this.mode
+
+      // Collected properties
+      let props = Object.assign({ boundary, toggle, mode }, this.options)
+
+      return JSON.stringify(props)
+    },
+  },
+}
+</script>
+<docs>
+```jsx
+<div>
+  <div class="uk-button-group">
+    <oc-button id="my_menu" text="Menu" />
+    <oc-button id="my_filter" text="Filter" />
+    <oc-button id="my_advanced" text="Advanced" />
+  </div>
+
+  <oc-drop toggle="#my_menu" mode="click">
+    <oc-nav>
+      <oc-nav-item icon="create_new_folder">Item with icon</oc-nav-item>
+      <oc-nav-item>Item without icon</oc-nav-item>
+      <oc-nav-item>Active item</oc-nav-item>
+    </oc-nav>
+  </oc-drop>
+
+  <oc-drop toggle="#my_filter" mode="hover">
+    <p>
+      Lets filter:
+    </p>
+    <ul class="uk-list">
+      <li>
+        <label><oc-checkbox /> <span class="uk-text-meta">Show Files</span></label>
+      </li>
+      <li>
+        <label><oc-checkbox /> <span class="uk-text-meta">Show Folders</span></label>
+      </li>
+      <li>
+        <oc-search-bar small placeholder="Filter by name" :button="false" />
+      </li>
+    </ul>
+  </oc-drop>
+
+  <oc-drop toggle="#my_advanced" mode="hover">
+    <div slot="special" class="uk-card uk-card-default">
+      <div class="uk-card-header">
+        <h3 class="uk-card-title">
+          Advanced
+        </h3>
+      </div>
+      <div class="uk-card-body">
+        <p>
+          I'm a slightly more advanced drop down
+        </p>
+      </div>
+    </div>
+  </oc-drop>
+</div>
+```
+</docs>
