7589910990c6ffcc52fd9b1ee4cb0bf6eb0f293f
Lukáš Hirt <hirtlu@gmail.com>
2019-04-18 16:41:56 +0200

    Created oc-dropzone component

---
+++ src/elements/_OCDropzone.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="oc-dropzone">
+    <span class="uk-text-lead">
+      <slot />
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "oc-dropzone",
+  status: "review",
+  release: "0.0.1",
+}
+</script>
+<docs>
+  ```jsx
+  <template>
+    <oc-dropzone>
+      Drag and drop to upload content into current folder
+    </oc-dropzone>
+  </template>
+  ```
+</docs>
