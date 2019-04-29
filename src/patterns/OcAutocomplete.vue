12c3fc847b23a11204f2fc4e0195824057cffcf4
Thomas Müller <thomas.mueller@tmit.eu>
2019-04-29 12:44:01 +0200

    Loading string can be set via a property + changing the model is now triggering an event Allow array of objects in autocomplete Rename variables and methods Introduce filter function property Allow to inject texts Bind selection to model Format examples

--- src/patterns/OcAutocomplete.vue
+++ src/patterns/OcAutocomplete.vue
@@ -7,38 +7,44 @@
       :disabled="disabled"
       @keydown.up.prevent="highlighted--"
       @keydown.down.prevent="highlighted++"
-      @keydown.enter="selectSuggestion"
+      @keydown.enter="$_ocAutocomplete_selectSuggestion"
     />
-    <div hidden :id="_boundryId" />
+    <div hidden :id="$_ocAutocomplete_boundryId" />
     <div
       class="oc-autocomplete-dropdown"
-      :uk-drop="'mode:click;delay-hide:0;toggle:#' + _boundryId"
-      :id="_dropdownId"
+      :uk-drop="'mode:click;delay-hide:0;toggle:#' + $_ocAutocomplete_boundryId"
+      :id="$_ocAutocomplete_dropdownId"
     >
       <ul class="oc-autocomplete-suggestion-list">
-        <template v-for="(item, i) in matchesShown">
+        <template v-for="(item, i) in $_ocAutocomplete_matchesShown">
           <li
             :class="[
               'oc-autocomplete-suggestion',
               { 'oc-autocomplete-suggestion-selected': i === highlighted },
             ]"
             :key="i"
-            v-text="item"
             @mouseenter="highlighted = i"
-            @click="selectSuggestion"
-          ></li>
+            @click="$_ocAutocomplete_selectSuggestion"
+          >
+            <slot name="item" v-bind:item="item">
+              <!-- Fallback content -->
+              {{ item }}
+            </slot>
+          </li>
         </template>
         <li
-          v-if="matchesOverflowing > 0 && !itemsLoading"
+          v-if="$_ocAutocomplete_matchesOverflowing > 0 && !itemsLoading"
           class="oc-autocomplete-suggestion-overflow"
         >
-          {{ matchesOverflowing }}
-          <span v-if="matchesOverflowing === 1">more result</span>
-          <span v-else>more results</span>
+          {{ $_ocAutocomplete_matchesOverflowing }}
+          <span v-if="$_ocAutocomplete_matchesOverflowing === 1">{{
+            $_ocAutocomplete_text.oneMoreResult
+          }}</span>
+          <span v-else>{{ $_ocAutocomplete_text.moreResults }}</span>
         </li>
         <li v-if="itemsLoading" class="oc-autocomplete-suggestion-list-loader">
           <oc-spinner class="oc-autocomplete-spinner" />
-          <span>{{ spinnerText }}</span>
+          <span>{{ $_ocAutocomplete_text.spinner }}</span>
         </li>
       </ul>
     </div>
@@ -56,11 +62,6 @@ import { uniqueId as _uniqueId } from "lodash"
  * and presenting the user a list of possible matches to the entered search term.
  *
  * The component supports single select only at the moment.
- *
- * ##TODO:
- *
- * - [ ] Allow complex content (HTML)
- * - [ ] Selecting model is not yet clean - will need to be changed together with ^
  */
 export default {
   name: "oc-autocomplete",
@@ -78,10 +79,11 @@ export default {
     /**
      * Informative text displayed right next to the spinner while loading data
      */
-    spinnerText: {
-      type: String,
-      required: false,
-      default: "Loading…",
+    text: {
+      type: Object,
+      default: function() {
+        return {}
+      },
     },
     /**
      * Input can be entered or not
@@ -114,10 +116,13 @@ export default {
       default: 5,
     },
     /**
-     * The selected element
+     * Select what values an object is to be found by
      */
-    value: {
-      type: String,
+    filter: {
+      type: Function,
+      default: (item, queryText) => {
+        return item.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1
+      },
     },
   },
   data() {
@@ -127,28 +132,39 @@ export default {
     }
   },
   computed: {
-    matchesShown() {
-      return this._matches.slice(0, this.maxListLength)
+    $_ocAutocomplete_matchesShown() {
+      return this.$_ocAutocomplete_matches.slice(0, this.maxListLength)
     },
-    matchesOverflowing() {
-      return this._matches.length - this.matchesShown.length
+    $_ocAutocomplete_matchesOverflowing() {
+      return this.$_ocAutocomplete_matches.length - this.$_ocAutocomplete_matchesShown.length
     },
-    _matches() {
+    $_ocAutocomplete_matches() {
       if (this.input.length === 0) {
         return []
       }
+
       return this.items.filter(item => {
-        return item.toLowerCase().indexOf(this.input.toLowerCase()) >= 0
+        let searchString = this.input // What's to be found
+        return this.filter(item, searchString)
       })
     },
-    _dropdownId() {
+    $_ocAutocomplete_dropdownId() {
       return _uniqueId("oc-autocomplete-dropdown-")
     },
-    _boundryId() {
+    $_ocAutocomplete_boundryId() {
       return _uniqueId("oc-autocomplete-boundry-")
     },
-    _dropdown() {
-      return UiKit.drop(`#${this._dropdownId}`)
+    $_ocAutocomplete_dropdown() {
+      return UiKit.drop(`#${this.$_ocAutocomplete_dropdownId}`)
+    },
+    $_ocAutocomplete_text() {
+      let text = {
+        spinner: "Loading…",
+        moreResults: "more results",
+        oneMoreResult: "1 more result",
+      }
+
+      return Object.assign(text, this.text)
     },
   },
   watch: {
@@ -157,39 +173,40 @@ export default {
 
       if (
         input.length === 0 ||
-        (input === this.matchesShown[0] && this.matchesShown.length === 1)
+        (input === this.$_ocAutocomplete_matchesShown[0] &&
+          this.$_ocAutocomplete_matchesShown.length === 1)
       ) {
-        this._dropdown.hide()
+        this.$_ocAutocomplete_dropdown.hide()
       } else {
-        this._dropdown.show()
+        this.$_ocAutocomplete_dropdown.show()
       }
 
       // The real update not depending on onblur
-      this.userInput(input)
+      this.$_ocAutocomplete_userInput(input)
     },
     highlighted(next, current) {
       if (next === current) return
 
       // come around
       if (next < 0) {
-        this.highlighted = this.matchesShown.length - 1
-      } else if (next > this.matchesShown.length - 1) {
+        this.highlighted = this.$_ocAutocomplete_matchesShown.length - 1
+      } else if (next > this.$_ocAutocomplete_matchesShown.length - 1) {
         this.highlighted = 0
       }
     },
   },
   methods: {
-    userInput(value) {
+    $_ocAutocomplete_userInput(value) {
       /**
        * This event is emitted as soon as the user changes the search term
        * @type {string}
        */
       this.$emit("update:input", value)
     },
-    selectSuggestion() {
-      if (this.matchesShown[this.highlighted]) {
-        this.input = this.matchesShown[this.highlighted]
-        this.$emit("input", this.input)
+    $_ocAutocomplete_selectSuggestion() {
+      if (this.$_ocAutocomplete_matchesShown[this.highlighted]) {
+        this.$emit("input", this.$_ocAutocomplete_matchesShown[this.highlighted])
+        this.input = ""
       }
     },
   },
@@ -202,18 +219,33 @@ export default {
     <h3 class="uk-heading-divider">
       Autocomplete
     </h3>
-    <div class="uk-margin">
-      <oc-autocomplete :items="[]" disabled placeholder="I am disabled" />
+    <div class="uk-card uk-card-default uk-card-small uk-card-body">
+      <oc-autocomplete v-model="simpleSelection" :items="simpleItems" placeholder="type 'le' for example results" />
+      <div class="uk-background-muted uk-padding-small uk-margin-small-top">
+        <p class="uk-text-meta">Selected simple item:</p>
+        <code>{{ simpleSelection }}</code>
+      </div>
     </div>
-    <div class="uk-margin">
-      <oc-autocomplete :items="items" placeholder="Add user" />
+    <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin-top">
+      <oc-autocomplete v-model="complexSelection" :items="complexItems" :filter="filterComplexItems" placeholder="type 'er' for example results">
+        <template v-slot:item="{item}">
+          <span>{{ item.forename }} {{ item.surname }} <em>(Age: {{ item.age }})</em></span>
+        </template>
+      </oc-autocomplete>
+      <div class="uk-background-muted uk-padding-small uk-margin-small-top">
+        <p class="uk-text-meta">Selected complex item:</p>
+        <code>{{ complexSelection }}</code>
+      </div>
     </div>
     <h3 class="uk-heading-divider">
-      Autocomplete in action
+      Autocomplete (delayed fetch)
     </h3>
-    <div class="uk-margin">
-      <oc-autocomplete v-model="selectedItem" :items="searchResult" :itemsLoading="searchInProgress" placeholder="Add user" @update:input="onInput"/>
-      <span>Selected item: {{ selectedItem }}</span>
+    <div class="uk-card uk-card-default uk-card-small uk-card-body uk-margin-top">
+      <oc-autocomplete v-model="delayedItem" :items="delayedResult" :itemsLoading="delayedSearchInProgress" placeholder="type 'le' and wait a little" @update:input="onInput"/>
+      <div class="uk-background-muted uk-padding-small uk-margin-small-top">
+        <p class="uk-text-meta">Selected complex item:</p>
+        <code>{{ delayedItem }}</code>
+      </div>
     </div>
   </section>
 </template>
@@ -221,7 +253,9 @@ export default {
 export default {
   data () {
     return {
-      items : [
+      // Simple example
+
+      simpleItems : [
         'Scott Mortensen',
         'Lecia Scheerer',
         'Merrie Rubin',
@@ -233,18 +267,51 @@ export default {
         'Verona Mounts',
         'Arlena Bolster'
       ],
-      searchResult: [],
-      searchInProgress: false,
-      selectedItem: null
+      simpleSelection : null,
+
+      // Complex example
+
+      complexItems : [{
+        id : 8312,
+        forename : 'Scott',
+        surname : 'Mortensen',
+        age : 31,
+      }, {
+        id : 7388,
+        forename : 'Lecia',
+        surname : 'Scheerer',
+        age : 21,
+      }, {
+        id : 992,
+        forename : 'Verona',
+        surname : 'Mounts',
+        age : 33,
+      }, {
+        id : 1211,
+        forename : 'Arlena',
+        surname : 'Bolster',
+        age : 66,
+      }],
+      complexSelection : {},
+
+      // delayed example
+
+      delayedResult: [],
+      delayedItem: null,
+      delayedSearchInProgress: false
     }
   },
   methods: {
     onInput() {
-      this.searchInProgress = true
+      this.delayedSearchInProgress = true
       setTimeout(() => {
-        this.searchInProgress = false
-        this.searchResult = this.items
+        this.delayedSearchInProgress = false
+        this.delayedResult = this.simpleItems
       }, 2000)
+    },
+    filterComplexItems(item, queryText) {
+      return item.forename.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1 ||
+              item.surname.toLocaleLowerCase().indexOf(queryText.toLocaleLowerCase()) > -1;
     }
   }
 }
