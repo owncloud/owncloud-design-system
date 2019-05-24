edea86dfa02affa49bf9b1501ec5ec22b3c7bbd6
Felix Heidecke <felix@heidecke.me>
2019-05-24 11:06:36 +0200

    Add optional home-item in breadcrumb

--- src/elements/OcBreadcrumb.vue
+++ src/elements/OcBreadcrumb.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="uk-breadcrumb">
-    <li v-if="firstIsHome">
-      <oc-icon name="home" @click="$_ocBreadcrumb_goHome" />
+    <li v-if="home">
+      <a @click="home"><oc-icon name="home"/></a>
     </li>
     <li v-for="(item, index) in items" :key="index">
       <router-link v-if="item.to" :to="item.to">{{ item.text }}</router-link>
@@ -29,14 +29,9 @@ export default {
       default: [],
       required: true,
     },
-    firstIsHome: {
-      type: Boolean,
+    home: {
+      type: [Boolean, Function],
       default: false,
-    },
-  },
-  methods: {
-    $_ocBreadcrumb_goHome() {
-      // Route to home
     },
   },
 }
@@ -44,20 +39,24 @@ export default {
 
 <docs>
 ```vue
-  <template>
-    <oc-breadcrumb :items="items"></oc-breadcrumb>
-  </template>
-  <script>
-    export default {
-      data: () => {
-        return {
-          items: [
-                  {text:'Home',to:'/'},
-            {text:'Folder',to:{path:'folder'}},
-            {text:'Subfolder', onClick:() => alert('Breadcrumb clicked!')}]
-        }
+<template>
+  <oc-breadcrumb :items="items" :home="proclaim"></oc-breadcrumb>
+</template>
+<script>
+  export default {
+    data: () => {
+      return {
+        items: [
+          {text:'Folder',to:{path:'folder'}},
+          {text:'Subfolder', onClick:() => alert('Breadcrumb clicked!')}]
+      }
+    },
+    methods : {
+      proclaim() {
+        alert("I'm going home")
       }
     }
-  </script>
+  }
+</script>
 ```
 </docs>
