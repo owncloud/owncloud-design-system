08c670704519ec6db75df2d455bd91819c635d0c
Thomas Müller <thomas.mueller@tmit.eu>
2020-04-02 16:08:33 +0200

    externalize uikit, vue-datetime, vue-avatar and moment

--- build/webpack.system.conf.js
+++ build/webpack.system.conf.js
@@ -32,6 +32,30 @@ const webpackConfig = merge(baseWebpackConfig, {
     library: "[name]",
     libraryTarget: config.system.libraryTarget,
   },
+  externals: [
+    "uikit", "vue-datetime", "vue-avatar", "moment"
+  ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace("@", "")}`
+          },
+        },
+      },
+    },
+  },
   performance: {
     hints: config.system.performanceHints,
   },
