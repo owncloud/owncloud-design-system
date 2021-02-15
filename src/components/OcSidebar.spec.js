9d5a52edb51f96dd724c0182c16fdfc66a70da26
Pascal Wengerter <pascal@wengerter.info>
2021-02-15 18:05:06 +0000

    Fix link to owncloud.com/wp-content/uploads/.. SVG

--- src/components/OcSidebar.spec.js
+++ src/components/OcSidebar.spec.js
@@ -12,7 +12,7 @@ const defaultProps = {
 
 describe("OcSidebar", () => {
   it('displays a logo image if its source is specified', () => {
-    const logoSrc = 'https://owncloud.org/wp-content/themes/owncloud/img/owncloud-org-logo.svg'
+    const logoSrc = 'https://owncloud.com/wp-content/uploads/2020/06/oc-logo-1c-1.svg'
     const wrapper = shallowMount(Sidebar, {
       propsData: {
         ...defaultProps,
