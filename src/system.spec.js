e36d1ba332e56aebcb94d70316572139c9f936e5
Pascal Wengerter <pascal@wengerter.info>
2021-03-23 22:40:13 +0000

    Create custom CSS props from themeOptions

---
+++ src/system.spec.js
@@ -0,0 +1,24 @@
+import Vue from "vue"
+import DesignSystem from "./system"
+
+let options = {
+  designTokens: {
+    colorPalette: {
+      "background": "#ef23ab",
+      "brand-primary": "#00FFFF",    
+    }
+  }
+}
+
+Vue.use(DesignSystem, options)
+
+describe("Depending on what gets passed into the theming options", () => {
+  it('Sets correct custom CSS props from theming options', () => {
+    expect(document.documentElement.style.getPropertyValue('--oc-background')).toMatch('#ef23ab')
+    expect(document.documentElement.style.getPropertyValue('--oc-brand-primary')).toMatch('#00FFFF')
+  })
+  it('Defaults to ODS colors where none are passed in theming options', () => {
+    expect(document.documentElement.style.getPropertyValue('--oc-color')).toMatch('#041E42')
+    expect(document.documentElement.style.getPropertyValue('--oc-brand-primary-hover')).toMatch('#223959')
+  })
+})
