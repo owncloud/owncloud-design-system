a1749459ae33dea4197b3b55171280ea52323dcc
Lukas Hirt <lukashirt@mail.com>
2021-04-06 17:02:28 +0200

    feat: add vue select

---
+++ src/components/OcSelect.vue
@@ -0,0 +1,182 @@
+<template>
+  <vue-select :value="model" class="oc-select" v-bind="$attrs" v-on="$listeners">
+    <template v-for="(index, name) in $scopedSlots" #[name]="data">
+      <slot :name="name" v-bind="data"></slot>
+    </template>
+  </vue-select>
+</template>
+
+<script>
+import VueSelect from "vue-select"
+import "vue-select/dist/vue-select.css"
+
+/**
+ * Select component with a trigger and dropdown based on [Vue Select](https://vue-select.org/)
+ */
+export default {
+  name: "OcSelect",
+
+  components: { VueSelect },
+
+  inheritAttrs: true,
+
+  model: {
+    prop: "model",
+    event: "input",
+  },
+
+  props: {
+    model: {
+      type: [Array, String, Object],
+      required: false,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.oc-select {
+  .vs {
+    &__search::placeholder,
+    &__dropdown-toggle,
+    &__dropdown-menu {
+      background: var(--oc-input-background);
+      border: 1px solid var(--oc-input-border);
+      color: var(--oc-input-color);
+    }
+
+    &__clear,
+    &__open-indicator {
+      fill: var(--oc-input-color);
+    }
+
+    &__dropdown-option--highlight {
+      background: var(--oc-input-color);
+    }
+
+    &__actions svg {
+      overflow: visible;
+    }
+
+    &__clear svg {
+      max-width: 10px;
+    }
+  }
+
+  &:focus-within {
+    .vs__dropdown-menu,
+    .vs__dropdown-toggle {
+      border: 1px solid var(--oc-input-color);
+    }
+  }
+}
+</style>
+
+<docs>
+For detailed documentation (props, slots, events, etc.), please visit https://vue-select.org/
+
+```vue
+<template>
+  <div class="uk-width-medium">
+    <oc-select v-model="selected" :options="['Apple', 'Bannana', 'Orange', 'Pear']" />
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    selected: 'Apple'
+  })
+}
+</script>
+```
+
+### Prevent user from clearing the selection
+If we want to disable the clear button from the `oc-select`, we can set prop `clearbable` to false. This will also prevent clearing the selected value by hitting `delete`.
+
+```vue
+<template>
+  <div class="uk-width-medium">
+    <oc-select v-model="selected" :options="['Apple', 'Bannana', 'Orange', 'Pear']" :clearable="false" />
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    selected: 'Apple'
+  })
+}
+</script>
+```
+
+### Disable search
+To prevent user from filtering options by typing a serach query into the `oc-select` component, set prop called `searchable` to false.
+
+```vue
+<template>
+  <div class="uk-width-medium">
+    <oc-select v-model="selected" :options="['Apple', 'Bannana', 'Orange', 'Pear']" :searchable="false" />
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    selected: 'Apple'
+  })
+}
+</script>
+```
+
+### Using slots to display complex options
+Sometimes we need to display more complex options. This can include e.g. an option with a title and a description. To still display all those values exactly as we want to, we need to use scoped slot called `option`.
+We can then retrieve all the values that we want to display from the slots parametres. It is important to specify the `label` prop on the `oc-select` component which will specify which key should be used as the option label.
+
+```vue
+<template>
+  <div class="uk-width-medium">
+    <oc-select v-model="selected" :options="options" label="title" class="oc-mb-m">
+      <template v-slot:option="{ title, desc }">
+        <span class="option">
+          <strong v-text="title" />
+        </span>
+        <span class="option" v-text="desc" />
+      </template>
+      <template #no-options>
+        Your search query haven't returned any results.
+      </template>
+    </oc-select>
+    <p>
+      Selected: {{ selected }}
+    </p>
+  </div>
+</template>
+<script>
+const options = [
+  {
+    title: 'Apple',
+    desc: 'An apple is an edible fruit produced by an apple tree (Malus domestica)'
+  },
+  {
+    title: 'Bannana',
+    desc: 'Bannana is a genus of goblin spiders (family Oonopidae) native to Xishuangbanna prefecture, Yunnan Province, China, where it lives in the leaf-litter of tropical rainforest'
+  },
+  {
+    title: 'Orange',
+    desc: 'The orange is the fruit of various citrus species in the family Rutaceae'
+  },
+]
+
+export default {
+  data: () => ({
+    selected: options[0],
+    options
+  })
+}
+</script>
+<style scoped>
+.option {
+  display: block;
+}
+</style>
+```
+</docs>
