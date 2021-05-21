4c63b82999c92c83b3867d187ea7bab4c85c24d9
Florian Schade <f.schade@icloud.com>
2021-05-21 13:24:20 +0200

    inline oc-alert styles

--- src/components/OcAlert.vue
+++ src/components/OcAlert.vue
@@ -73,6 +73,43 @@ export default {
 }
 </script>
 
+<style lang="scss">
+@mixin oc-alert-variation($color) {
+  background-color: $color;
+
+  &,
+  * {
+    color: var(--oc-color-text-inverse);
+  }
+
+  svg {
+    fill: var(--oc-color-text-inverse);
+  }
+}
+
+.uk-alert.oc-alert {
+  &-passive {
+    @include oc-alert-variation(var(--oc-color-swatch-passive-default));
+  }
+
+  &-primary {
+    @include oc-alert-variation(var(--oc-color-swatch-primary-default));
+  }
+
+  &-success {
+    @include oc-alert-variation(var(--oc-color-swatch-success-default));
+  }
+
+  &-warning {
+    @include oc-alert-variation(var(--oc-color-swatch-warning-default));
+  }
+
+  &-danger {
+    @include oc-alert-variation(var(--oc-color-swatch-danger-default));
+  }
+}
+</style>
+
 <docs>
 ```vue
 <div>
