64735cd5471869991338f8c64596f7ff3cdee4f4
Tobias Baader <tbaader@owncloud.com>
2021-05-21 17:05:38 +0200

    reduce fileslist row height
    setting the row height to the minimum possible with the sharing-indicators still placed below the filename.

--- src/components/table/_OcTableCell.vue
+++ src/components/table/_OcTableCell.vue
@@ -55,7 +55,7 @@ export default {
 <style lang="scss">
 .oc-table-cell {
   /* padding is not configurable until we need it */
-  padding: var(--oc-space-xsmall);
+  padding: 0px var(--oc-space-xsmall);
   position: relative;
 
   &-align {
