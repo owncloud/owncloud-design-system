c6fbebc8c40b97eb4f344176233fffcfe53f9a14
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-28 11:49:37 +0545

    Reuse selectors and wrapper getters

--- src/components/OcActionDrop.spec.js
+++ src/components/OcActionDrop.spec.js
@@ -2,29 +2,38 @@ import OcActionDrop from "./OcActionDrop.vue"
 import { mount } from "@vue/test-utils"
 
 describe("OcActionDrop", () => {
+  const selectors = {
+    dropdownMenu: ".oc-dropdown-menu",
+    dropdownWrapperButton: ".oc-dropdown-wrapper button",
+  }
+  const defaultActionSlot = {
+    actions: "<p class='slot-action'>First action</p>" + "<p class='slot-action'>Second action</p>",
+  }
+  function getWrapperWithActionSlot() {
+    return mount(OcActionDrop, { slots: defaultActionSlot })
+  }
   describe("toggle dropdown", () => {
     let wrapper
     beforeEach(async () => {
-      wrapper = await mount(OcActionDrop, {
-        slots: {
-          actions:
-            "<p class='slot-action'>First action</p>" + "<p class='slot-action'>Second action</p>",
-        },
-      })
+      wrapper = await getWrapperWithActionSlot()
     })
     it("should set dropdown menu as hidden at first", () => {
-      expect(wrapper.find(".oc-dropdown-menu").exists()).toBeTruthy()
-      expect(wrapper.find(".oc-dropdown-wrapper button").attributes("aria-expanded")).toBe("false")
-      expect(wrapper.find(".oc-dropdown-menu").attributes("hidden")).toBe("hidden")
+      expect(wrapper.find(selectors.dropdownMenu).exists()).toBeTruthy()
+      expect(wrapper.find(selectors.dropdownWrapperButton).attributes("aria-expanded")).toBe(
+        "false"
+      )
+      expect(wrapper.find(selectors.dropdownMenu).attributes("hidden")).toBe("hidden")
     })
     describe("when toggle button is toggled", () => {
       it("should toggle visibility of dropdown menu", async () => {
-        await wrapper.find(".oc-dropdown-wrapper button").trigger("click")
-        expect(wrapper.find(".oc-dropdown-menu").attributes("hidden")).toBe(undefined)
-        expect(wrapper.find(".oc-dropdown-wrapper button").attributes("aria-expanded")).toBe("true")
-        await wrapper.find(".oc-dropdown-wrapper button").trigger("click")
-        expect(wrapper.find(".oc-dropdown-menu").attributes("hidden")).toBe("hidden")
-        expect(wrapper.find(".oc-dropdown-wrapper button").attributes("aria-expanded")).toBe(
+        await wrapper.find(selectors.dropdownWrapperButton).trigger("click")
+        expect(wrapper.find(selectors.dropdownMenu).attributes("hidden")).toBe(undefined)
+        expect(wrapper.find(selectors.dropdownWrapperButton).attributes("aria-expanded")).toBe(
+          "true"
+        )
+        await wrapper.find(selectors.dropdownWrapperButton).trigger("click")
+        expect(wrapper.find(selectors.dropdownMenu).attributes("hidden")).toBe("hidden")
+        expect(wrapper.find(selectors.dropdownWrapperButton).attributes("aria-expanded")).toBe(
           "false"
         )
       })
@@ -42,26 +51,22 @@ describe("OcActionDrop", () => {
         wrapper.vm.$refs.button.focus = jest.fn()
       })
       it("should close the dropdown menu", async () => {
-        await wrapper.find(".oc-dropdown-menu").trigger("click")
-        expect(wrapper.find(".oc-dropdown-menu").attributes("hidden")).toBe("hidden")
+        expect(wrapper.find(selectors.dropdownMenu).attributes("hidden")).toBe(undefined)
+        await wrapper.find(selectors.dropdownMenu).trigger("click")
+        expect(wrapper.find(selectors.dropdownMenu).attributes("hidden")).toBe("hidden")
       })
     })
   })
   describe("open menu and focus", () => {
     let wrapper
     beforeEach(async () => {
-      wrapper = await mount(OcActionDrop, {
-        slots: {
-          actions:
-            "<p class='slot-action'>First action</p>" + "<p class='slot-action'>Second action</p>",
-        },
-      })
+      wrapper = await getWrapperWithActionSlot()
     })
     it.each(["keydown.enter", "keydown.space", "keydown.down"])(
       "should open the dropdown menu on different keyboard events",
       async event => {
-        await wrapper.find(".oc-dropdown-wrapper button").trigger(event)
-        expect(wrapper.find(".oc-dropdown-menu").attributes("hidden")).toBe(undefined)
+        await wrapper.find(selectors.dropdownWrapperButton).trigger(event)
+        expect(wrapper.find(selectors.dropdownMenu).attributes("hidden")).toBe(undefined)
       }
     )
   })
