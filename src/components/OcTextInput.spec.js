61d5dc67228c37e7f34ac6507955a01249d33921
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-09 14:33:04 +0545

    Fixed test descriptions

--- src/components/OcTextInput.spec.js
+++ src/components/OcTextInput.spec.js
@@ -52,47 +52,47 @@ describe("OcTextInput", () => {
     })
   })
 
-  describe("when description message is provided", () => {
+  describe("when a description message is provided", () => {
     const wrapper = getShallowWrapper({ descriptionMessage: "You should pass." })
-    it("should add description class to input message", () => {
+    it("should add the description class to the input message", () => {
       expect(wrapper.find(selectors.textInputMessage).attributes().class).toContain(
         "oc-text-input-description"
       )
     })
-    it("should show the description message as input message text", () => {
+    it("should show the description message as the input message text", () => {
       expect(wrapper.find(selectors.textInputMessage).text()).toBe("You should pass.")
     })
   })
 
-  describe("when warning message is provided", () => {
+  describe("when a warning message is provided", () => {
     const wrapper = getShallowWrapper({ warningMessage: "You may pass." })
-    it("should add warning class to input", () => {
+    it("should add the warning class to the input", () => {
       expect(wrapper.find("input").attributes().class).toContain("oc-text-input-warning")
     })
-    it("should add warning class to input message", () => {
+    it("should add the warning class to the input message", () => {
       expect(wrapper.find(selectors.textInputMessage).attributes().class).toContain(
         "oc-text-input-warning"
       )
     })
-    it("should show the warning message as input message text", () => {
+    it("should show the warning message as the input message text", () => {
       expect(wrapper.find(selectors.textInputMessage).text()).toBe("You may pass.")
     })
   })
 
-  describe("when error message is provided", () => {
+  describe("when an error message is provided", () => {
     const wrapper = getShallowWrapper({ errorMessage: "You shall not pass." })
-    it("should add error class to input", () => {
+    it("should add the error class to the input", () => {
       expect(wrapper.find("input").attributes().class).toContain("oc-text-input-danger")
     })
-    it("should add error class to input message", () => {
+    it("should add the error class to the input message", () => {
       expect(wrapper.find(selectors.textInputMessage).attributes().class).toContain(
         "oc-text-input-danger"
       )
     })
-    it("should show the error message as input message text", () => {
+    it("should show the error message as the input message text", () => {
       expect(wrapper.find(selectors.textInputMessage).text()).toBe("You shall not pass.")
     })
-    it("should set input aria-invalid as true", () => {
+    it("should set the input aria-invalid attribute to true", () => {
       expect(wrapper.find("input").attributes("aria-invalid")).toBe("true")
     })
   })
@@ -122,7 +122,7 @@ describe("OcTextInput", () => {
   })
 
   describe("type prop", () => {
-    it("should not allow types other than text, number, email and password", () => {
+    it("should only allow text, number, email and password as type", () => {
       try {
         getShallowWrapper({ type: "binary" })
         throw new Error(`Provided value 'binary' for prop 'type' is valid.`)
@@ -134,7 +134,7 @@ describe("OcTextInput", () => {
       }
     })
     it.each(["text", "number", "email", "password"])(
-      "should set provided type for the input",
+      "should set the provided type for the input",
       type => {
         const wrapper = getShallowWrapper({ type: type })
         expect(wrapper.find("input").attributes("type")).toBe(type)
@@ -143,7 +143,7 @@ describe("OcTextInput", () => {
   })
 
   describe("input events", () => {
-    it("should emit input event on typing", async () => {
+    it("should emit an input event on typing", async () => {
       const wrapper = getShallowWrapper()
       expect(wrapper.emitted().input).toBeFalsy()
       await wrapper.find("input").setValue("a")
@@ -152,8 +152,8 @@ describe("OcTextInput", () => {
     })
   })
 
-  describe("clear button accessible label prop", () => {
-    it("should set aria label of clear input button if provided", () => {
+  describe("clear-button accessible label prop", () => {
+    it("should set the aria label attribute if provided", () => {
       const wrapper = getShallowWrapper({
         clearButtonEnabled: true,
         value: "non-empty-value",
