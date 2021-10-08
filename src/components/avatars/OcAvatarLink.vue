c2389ed391102b95a8f8ddb8977193447be4dedb
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-08 18:39:12 +0200

    chore(OcAvatarLink): change file path

--- src/components/avatars/OcAvatarLink.vue
+++ src/components/avatars/OcAvatarLink.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-import OcAvatarItem from "./OcAvatarItem"
+import OcAvatarItem from "../avatars/OcAvatarItem/OcAvatarItem.vue"
 export default {
   name: "OcAvatarLink",
   status: "ready",
