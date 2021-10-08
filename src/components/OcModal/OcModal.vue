c702244457c2a2509683eba2fcff42aa23027982
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-08 18:08:35 +0200

    chore(OcModal): change file path

--- src/components/OcModal/OcModal.vue
+++ src/components/OcModal/OcModal.vue
@@ -56,7 +56,7 @@
 </template>
 
 <script>
-import OcButton from "../OcButton.vue"
+import OcButton from "../OcButton/OcButton.vue"
 import OcIcon from "../OcIcon.vue"
 import OcTextInput from "../OcTextInput/OcTextInput.vue"
 import { FocusTrap } from "focus-trap-vue"
