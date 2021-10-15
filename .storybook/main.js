37c233d3dcd9bbc7cb13d56708d5df1c70196efb
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-15 12:30:32 +0200

    Create main.js

---
+++ .storybook/main.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-jest",
+    "addon-screen-reader",
+  ],
+  webpackFinal: async (storybookWebpackConfig, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Return the altered config
+    return merge(storybookWebpackConfig, {
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+              { loader: "vue-style-loader" },
+              { loader: "css-loader" },
+              { loader: "postcss-loader" },
+              { loader: "sass-loader" },
+              {
+                loader: "sass-resources-loader", options: {
+                  resources: [
+                    path.resolve(__dirname, "../src/assets/tokens/ods.scss"),
+                    path.resolve(__dirname, "../src/styles/styles.scss")
+                  ]
+                }
+              }
+            ],
+            include: path.resolve(__dirname, "../")
+          }
+        ]
+      }
+    });
+  }
+};
+
