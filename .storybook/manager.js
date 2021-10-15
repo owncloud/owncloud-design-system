fa957d6128152260bc72810a02cef0e37c8ab25e
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-15 12:30:34 +0200

    Create manager.js

---
+++ .storybook/manager.js
@@ -0,0 +1,11 @@
+import { addons } from "@storybook/addons"
+import { themes } from "@storybook/theming"
+// import customTheme from "./customTheme"
+
+// addons.setConfig({
+//   theme: customTheme,
+// })
+
+addons.setConfig({
+  theme: themes.light,
+})
