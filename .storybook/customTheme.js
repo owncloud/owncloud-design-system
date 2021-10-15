a0a48164f6b343a605a03757ec1601f645816973
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-15 12:30:29 +0200

    Create customTheme.js

---
+++ .storybook/customTheme.js
@@ -0,0 +1,37 @@
+import { create } from "@storybook/theming"
+
+export default create({
+  base: "light",
+
+  colorPrimary: "hotpink",
+  colorSecondary: "deepskyblue",
+
+  // UI
+  appBg: "white",
+  appContentBg: "silver",
+  appBorderColor: "grey",
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: "black",
+  textInverseColor: "rgba(255,255,255,0.9)",
+
+  // Toolbar default and active colors
+  barTextColor: "silver",
+  barSelectedColor: "black",
+  barBg: "hotpink",
+
+  // Form colors
+  inputBg: "white",
+  inputBorder: "silver",
+  inputTextColor: "black",
+  inputBorderRadius: 4,
+
+  brandTitle: "ownCloud Storybook",
+  brandUrl: "https://example.com",
+  brandImage: "https://samhobbs.co.uk/sites/default/files/owncloud-logo4.png",
+})
