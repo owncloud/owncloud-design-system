60bcc2e253f904aac95e7b2af2c3b47bea5f83af
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-18 08:24:48 +0200

    feat(main): added file path for documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
-const path = require("path");
-const { merge } = require("webpack-merge");
+const path = require("path")
+const { merge } = require("webpack-merge")
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../docs/**/*.stories.mdx",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -27,19 +31,19 @@ module.exports = {
               { loader: "postcss-loader" },
               { loader: "sass-loader" },
               {
-                loader: "sass-resources-loader", options: {
+                loader: "sass-resources-loader",
+                options: {
                   resources: [
                     path.resolve(__dirname, "../src/assets/tokens/ods.scss"),
-                    path.resolve(__dirname, "../src/styles/styles.scss")
-                  ]
-                }
-              }
+                    path.resolve(__dirname, "../src/styles/styles.scss"),
+                  ],
+                },
+              },
             ],
-            include: path.resolve(__dirname, "../")
-          }
-        ]
-      }
-    });
-  }
-};
-
+            include: path.resolve(__dirname, "../"),
+          },
+        ],
+      },
+    })
+  },
+}
