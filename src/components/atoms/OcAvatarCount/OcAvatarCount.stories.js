15b31f4ebf12317b7ec81180ca624856dc4c80e1
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-19 13:05:26 +0200

    Create OcAvatarCount.stories.js

---
+++ src/components/atoms/OcAvatarCount/OcAvatarCount.stories.js
@@ -0,0 +1,28 @@
+import OcAvatarCount from "./OcAvatarCount.vue"
+
+export default {
+  component: OcAvatarCount,
+  title: "Atoms/Avatar Count",
+  argTypes: {
+    count: {
+      control: { type: "number" },
+      required: true,
+    },
+    size: {
+      control: { type: "number" },
+      required: false,
+      defaultValue: 30,
+    },
+  },
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { OcAvatarCount },
+  template: `<oc-avatar-count v-bind="$props" />`,
+})
+
+export const Component = Template.bind({})
+Component.args = {
+  count: 42,
+}
