892f1b11413c9b4ea0dc4030f24207aca66c5498
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-22 08:48:20 +0200

    feat(manager): deactivated for testing purposes

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,11 +1,11 @@
-import { addons } from "@storybook/addons"
-import { themes } from "@storybook/theming"
+// import { addons } from "@storybook/addons"
+// import { themes } from "@storybook/theming"
 // import customTheme from "./customTheme"
 
 // addons.setConfig({
 //   theme: customTheme,
 // })
 
-addons.setConfig({
-  theme: themes.light,
-})
+// addons.setConfig({
+//   theme: themes.light,
+// })
