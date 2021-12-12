78e305fe10a08861ef15184416f571840cf0dfd7
Dominik Schmidt <dev@dominik-schmidt.de>
2021-12-12 18:04:20 +0100

    Extract sort handling from OcTable
    The sorting depends on the structure of resources which do not exist in ODS but only in web-app-files.
    Moreover, we need to sort before pagination and as pagination is not handled in OcTable either, we need to handle sorting externally as well.
    This has the additional benefit that this prepares us better for retrieving server sorted and paginated resources.

--- src/mixins/sort.js
+++ src/mixins/sort.js
@@ -5,76 +5,48 @@ const SORT_DIRECTION_ASC = "asc"
 const SORT_DIRECTION_DESC = "desc"
 
 export default {
-  data: () => ({
-    sortBy: null,
-    sortDir: SORT_DIRECTION_ASC,
-  }),
+  props: {
+    /**
+     * Show that the table is sorted by this column (no actual sorting takes place)
+     */
+    sortBy: {
+      type: String,
+      required: false,
+    },
+
+    /**
+     * Show that the table is sorted ascendingly/descendingly (no actual sorting takes place)
+     */
+    sortDir: {
+      type: String,
+      required: false,
+      default: undefined,
+      validator: value => {
+        return [SORT_DIRECTION_ASC, SORT_DIRECTION_DESC].includes(value)
+      },
+    },
+  },
   created() {
     if (this.isSortable) {
-      this.sortBy = this.firstSortableField
       this.$on(EVENT_THEAD_CLICKED, this.handleSort)
     }
   },
   computed: {
-    sortedData() {
-      if (!this.isSortable || !this.sortBy) {
-        return
-      }
-      if (this.sortBy === "name") {
-        const folders = [...this.data.filter(i => i.type === "folder")].sort((a, b) =>
-          this.sortData(a, b)
-        )
-        const files = [...this.data.filter(i => i.type !== "folder")].sort((a, b) =>
-          this.sortData(a, b)
-        )
-        if (this.sortDir === SORT_DIRECTION_ASC) {
-          return folders.concat(files)
-        }
-        return files.concat(folders)
-      }
-      return [...this.data].sort((a, b) => this.sortData(a, b))
-    },
     isSortable() {
       return this.fields.some(f => f.sortable)
     },
-    firstSortableField() {
-      const sortableFields = this.fields.filter(f => f.sortable).map(f => f.name)
-      if (sortableFields) {
-        return sortableFields[0]
-      }
-      return null
-    },
   },
   methods: {
-    sortData(a, b) {
-      let aValue = a[this.sortBy]
-      let bValue = b[this.sortBy]
-      const modifier = this.sortDir === SORT_DIRECTION_ASC ? 1 : -1
-
-      const { sortable } = this.fields.find(f => f.name === this.sortBy)
-
-      if (sortable) {
-        if (typeof sortable === "string") {
-          const genArrComp = vals => {
-            return vals.map(val => val[sortable]).join("")
-          }
-
-          aValue = genArrComp(aValue)
-          bValue = genArrComp(bValue)
-        } else if (typeof sortable === "function") {
-          aValue = sortable(aValue)
-          bValue = sortable(bValue)
-        }
+    extractSortThProps(props, field) {
+      if (!this.fieldIsSortable(field)) {
+        return
       }
 
-      if (!isNaN(aValue) && !isNaN(bValue)) {
-        return (aValue - bValue) * modifier
+      let sort = "none"
+      if (this.sortBy === field.name) {
+        sort = this.sortDir === SORT_DIRECTION_ASC ? "ascending" : "descending"
       }
-      const userLang = navigator.language || navigator.userLanguage
-      const compare = aValue
-        .toString()
-        .localeCompare(bValue.toString(), userLang, { sensitivity: "base" })
-      return compare * modifier
+      props["aria-sort"] = sort
     },
     fieldIsSortable({ sortable }) {
       return !!sortable
@@ -84,24 +56,22 @@ export default {
         return
       }
 
-      if (field.name === this.sortBy) {
-        this.sortDir =
-          this.sortDir === SORT_DIRECTION_ASC ? SORT_DIRECTION_DESC : SORT_DIRECTION_ASC
-        return
+      // only toggle sortDir if already sorted by this column
+      let sortDir = this.sortDir
+      if (field.name === this.sortBy || this.sortDir === undefined) {
+        sortDir = this.sortDir === SORT_DIRECTION_DESC ? SORT_DIRECTION_ASC : SORT_DIRECTION_DESC
       }
 
-      this.sortBy = field.name
-    },
-    extractSortThProps(props, field) {
-      if (!this.fieldIsSortable(field)) {
-        return
-      }
-
-      let sort = "none"
-      if (this.sortBy === field.name) {
-        sort = this.sortDir === SORT_DIRECTION_ASC ? "descending" : "ascending"
-      }
-      props["aria-sort"] = sort
+      /**
+       * Triggers when table heads are clicked
+       *
+       * @property {string} sortBy requested column to sort by
+       * @property {string} sortDir requested order to sort in (either asc or desc)
+       */
+      this.$emit("sort", {
+        sortBy: field.name,
+        sortDir,
+      })
     },
   },
 }
