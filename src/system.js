c29209633d3afb430058da10ff276d6116cd6432
Florian Schade <f.schade@icloud.com>
2022-01-15 22:48:52 +0100

    fix linter issues

--- src/system.js
+++ src/system.js
@@ -1,26 +1,26 @@
-import "./styles/styles.scss";
-import atoms from "./components/atoms/**/*.vue";
-import molecules from "./components/molecules/**/*.vue";
-import organisms from "./components/organisms/**/*.vue";
-import directivesContext from "./directives/**/*.js";
+import "./styles/styles.scss"
+import atoms from "./components/atoms/**/*.vue"
+import molecules from "./components/molecules/**/*.vue"
+import organisms from "./components/organisms/**/*.vue"
+import directivesContext from "./directives/**/*.js"
 
 const componentsContext = [...atoms, ...organisms, ...molecules]
 
 const initializeCustomProps = (tokens = [], prefix) => {
   for (const param in tokens) {
-    document.querySelector(":root").style.setProperty("--oc-" + prefix + param, tokens[param]);
+    document.querySelector(":root").style.setProperty("--oc-" + prefix + param, tokens[param])
   }
-};
+}
 
 // Install the above defined components
 const System = {
   install(Vue, options = {}) {
-    const themeOptions = options.tokens;
-    initializeCustomProps(themeOptions?.breakpoints, "breakpoint-");
-    initializeCustomProps(themeOptions?.colorPalette, "color-");
-    initializeCustomProps(themeOptions?.fontSizes, "font-size-");
-    initializeCustomProps(themeOptions?.sizes, "size-");
-    initializeCustomProps(themeOptions?.spacing, "space-");
+    const themeOptions = options.tokens
+    initializeCustomProps(themeOptions?.breakpoints, "breakpoint-")
+    initializeCustomProps(themeOptions?.colorPalette, "color-")
+    initializeCustomProps(themeOptions?.fontSizes, "font-size-")
+    initializeCustomProps(themeOptions?.sizes, "size-")
+    initializeCustomProps(themeOptions?.spacing, "space-")
 
     componentsContext.forEach(c => Vue.component(c.default.name, c.default))
     directivesContext.forEach(d => Vue.directive(d.default.name, d.default))
