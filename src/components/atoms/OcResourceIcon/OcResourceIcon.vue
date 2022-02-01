a4cf32cf8c60ac5b5520d8ef17c4f80b3481caba
Paul Neubauer <paulneubauer@live.de>
2022-02-01 11:04:04 +0100

    add ocresourceicon, theming

---
+++ src/components/atoms/OcResourceIcon/OcResourceIcon.vue
@@ -0,0 +1,32 @@
+<template>
+	<oc-icon 
+		key="resource-icon" 
+		:name="resource.icon"
+		:color="resource.iconColor"
+		size="large" />
+</template>
+
+<script>
+import OcIcon from "../../atoms/OcIcon/OcIcon.vue"
+
+export default {
+	name: "OcResourceIcon",
+  status: "ready",
+  release: "2.1.0",
+	components: { OcIcon },
+	props: { 
+		resource: {
+			type: Object,
+			required: true,
+		}
+	},
+	computed: {
+		iconName() {
+			return this.resource.icon;
+		},
+		iconColor() {
+			return this.resource.iconColor;
+		}
+	}
+}
+</script>
