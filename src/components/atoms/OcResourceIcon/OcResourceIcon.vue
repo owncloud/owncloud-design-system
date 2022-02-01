febc436987b55a0c5640a63a1ef0e17921b2e508
Paul Neubauer <paulneubauer@live.de>
2022-02-01 11:44:04 +0100

    add mapping (still needs to be refactored)

--- src/components/atoms/OcResourceIcon/OcResourceIcon.vue
+++ src/components/atoms/OcResourceIcon/OcResourceIcon.vue
@@ -8,6 +8,8 @@
 
 <script>
 import OcIcon from "../../atoms/OcIcon/OcIcon.vue"
+import iconExtensionMapping from "../../../helpers/resourceIconExtensionMapping"
+import iconColorExtensionMapping from "../../../helpers/resourceIconColorExtensionMapping"
 
 export default {
 	name: "OcResourceIcon",
@@ -22,10 +24,30 @@ export default {
 	},
 	computed: {
 		iconName() {
-			return this.resource.icon;
+			return this.getResourceIcon(this.resource.icon);
 		},
 		iconColor() {
-			return this.resource.iconColor;
+			return this.getResourceIconColor(this.resource.iconColor);
+		}
+	},
+	methods: {
+		getResourceIcon(extension) {
+			const icon = iconExtensionMapping[extension.toLowerCase()]
+
+			if (icon) {
+				return `resource-type-${icon}`
+			}
+
+			return 'resource-type-file'
+		},
+		getResourceIconColor(extension) {
+			const color = iconColorExtensionMapping[extension.toLowerCase()]
+
+			if (color) {
+				return color
+			}
+
+			return 'var(--oc-color-text-default)'
 		}
 	}
 }
