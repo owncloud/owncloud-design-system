f35daff0f83cac0556a55215b794e38b3e7500f4
Florian Schade <f.schade@icloud.com>
2022-02-22 14:21:41 +0100

    Revert "extend modal focus-trap (#1969)"
    This reverts commit f9a3b22ec97ed6903e8d0262ea5b5c1d242b738f.

--- src/components/organisms/OcModal/OcModal.vue
+++ src/components/organisms/OcModal/OcModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="oc-modal-background" aria-labelledby="oc-modal-title">
-    <focus-trap :active="focusTrapEnabled" :initial-focus="focusTrapInitial">
+    <focus-trap :active="true" :initial-focus="getInitialFocusRef">
       <div
         ref="ocModal"
         :class="classes"
@@ -230,24 +230,6 @@ export default {
       required: false,
       default: false,
     },
-
-    /**
-     * Asserts whether the modal should be protected with focus trap
-     */
-    focusTrapEnabled: {
-      type: Boolean,
-      required: false,
-      default: true,
-    },
-    /**
-     * Overwrite default focused element
-     * Can be `#id, .class`.
-     */
-    focusTrapInitial: {
-      type: String,
-      required: false,
-      default: null,
-    },
   },
   data() {
     return {
@@ -295,7 +277,10 @@ export default {
       this.userInputValue = value
     },
     getInitialFocusRef() {
-      return this.focusTrapInitial || this.$refs.ocModalInput || this.$refs.ocModal
+      if (this.hasInput) {
+        return this.$refs.ocModalInput
+      }
+      return this.$refs.ocModal
     },
   },
 }
