d5608c24a511a6fb6258c6ec2daf1d2c558d22f1
Paul Neubauer <paulneubauer@live.de>
2022-03-08 16:07:07 +0100

    Redesign OcStatusIndicators

--- src/components/molecules/OcStatusIndicators/OcStatusIndicators.vue
+++ src/components/molecules/OcStatusIndicators/OcStatusIndicators.vue
@@ -6,20 +6,16 @@
         :id="indicator.id"
         :key="indicator.id"
         v-oc-tooltip="indicator.label"
-        class="oc-status-indicators-indicator"
+        class="oc-status-indicators-indicator oc-background-primary-gradient oc-p-xs oc-ml-xs"
         :aria-label="indicator.label"
         :aria-describedby="getIndicatorDescriptionId(indicator)"
         appearance="raw"
+        variation="inverse"
         :data-testid="indicator.id"
         :data-test-indicator-type="indicator.type"
         @click="indicator.handler(resource, indicator.target)"
       >
-        <oc-icon
-          :name="indicator.icon"
-          size="small"
-          :fill-type="indicator.fillType"
-          variation="passive"
-        />
+        <oc-icon :name="indicator.icon" size="small" fill-type="line" variation="passive" />
       </oc-button>
       <oc-icon
         v-else
@@ -122,6 +118,10 @@ export default {
 .oc-status-indicators {
   align-items: center;
   display: flex;
+  float: right;
+  &-indicator {
+    border-radius: 99px;
+  }
 }
 </style>
 
