779e842fb9a7f0da88382aeaf54e6fdb4444c2d7
Florian Schade <f.schade@icloud.com>
2022-03-21 09:37:08 +0100

    export package members

--- src/system.js
+++ src/system.js
@@ -6,10 +6,27 @@
  */
 
 import "./styles/styles.scss"
+import { getSizeClass } from "./utils/sizeClasses.js"
 
-// Define contexts to require
-const componentsContext = require.context("./components/", true, /\.vue$/)
-const directivesContext = require.context("./directives/", true, /\.js$/)
+const exportContext = ctx =>
+  ctx.keys().reduce((acc, key) => {
+    const exp = ctx(key).default || ctx(key)
+    const name =
+      exp.name ||
+      key
+        .split("/")
+        .pop()
+        .replace(/\.[^/.]+$/, "")
+    acc[name] = exp
+    return acc
+  }, {})
+
+export * as composables from "./composables"
+export const utils = { getSizeClass }
+export const components = exportContext(require.context("./components/", true, /\.vue$/))
+export const directives = exportContext(require.context("./directives/", true, /\.js$/))
+export const helpers = exportContext(require.context("./helpers/", true, /\.js$/))
+export const mixins = exportContext(require.context("./mixins/", true, /\.js$/))
 
 const initializeCustomProps = (tokens = [], prefix) => {
   for (const param in tokens) {
@@ -27,16 +44,8 @@ const System = {
     initializeCustomProps(themeOptions?.sizes, "size-")
     initializeCustomProps(themeOptions?.spacing, "space-")
 
-    componentsContext
-      .keys()
-      .forEach(key =>
-        Vue.component(componentsContext(key).default.name, componentsContext(key).default)
-      )
-    directivesContext
-      .keys()
-      .forEach(key =>
-        Vue.directive(directivesContext(key).default.name, directivesContext(key).default)
-      )
+    Object.keys(components).forEach(name => Vue.component(name, components[name]))
+    Object.keys(directives).forEach(name => Vue.directive(name, directives[name]))
   },
 }
 
