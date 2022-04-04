76f26f1552aa6e7b92306617725e6e4f7aecb129
CommanderRoot <CommanderRoot@users.noreply.github.com>
2022-04-04 22:38:59 +0200

    refactor: replace deprecated String.prototype.substr() (#2059)
    * refactor: replace deprecated String.prototype.substr()
    
    .substr() is deprecated so we replace it with functions which work similarily but aren't deprecated
    
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>
    
    * Add changelog
    
    * Fix changelog

--- src/components/molecules/OcTable/OcTable.vue
+++ src/components/molecules/OcTable/OcTable.vue
@@ -711,7 +711,7 @@ export default {
             const hours = date.getHours()
             const minutes = "0" + date.getMinutes()
             const seconds = "0" + date.getSeconds()
-            return hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2)
+            return hours + ":" + minutes.slice(-2) + ":" + seconds.slice(-2)
           }
         }]
       },
