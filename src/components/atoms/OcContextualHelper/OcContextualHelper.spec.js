ead889bfad09453f0a40eb5dbead36aca0c48dc1
Pascal Wengerter <pascal@wengerter.info>
2022-05-24 20:36:21 +0200

    Satisfy linter on existing master

--- src/components/atoms/OcContextualHelper/OcContextualHelper.spec.js
+++ src/components/atoms/OcContextualHelper/OcContextualHelper.spec.js
@@ -23,7 +23,7 @@ describe("OcContextualHelper", () => {
       const wrapper = getWrapperWithProps({ text: "test-my-text" })
       expect(wrapper.find(".info-text").text()).toBe("test-my-text")
     })
-    it("should set list prop", async () => {
+    it("should set list prop", () => {
       const listValues = ["a-list-value", "b-list-value", "c-list-value"]
       const wrapper = getWrapperWithProps({ list: listValues })
       const result = wrapper.find(".info-list").text()
@@ -31,13 +31,13 @@ describe("OcContextualHelper", () => {
         expect(result).toContain(value)
       })
     })
-    it("should set a readMore link", async () => {
+    it("should set a readMore link", () => {
       const wrapper = getWrapperWithProps({ readMoreLink: "owncloud.design" })
       const attributes = wrapper.find(".info-more-link").attributes()
       expect(attributes["href"]).toBe("owncloud.design")
       expect(attributes["target"]).toBe("_blank")
     })
-    it("should set end-text prop", async () => {
+    it("should set end-text prop", () => {
       const wrapper = getWrapperWithProps({ endText: "test-my-text" })
       expect(wrapper.find(".info-text-end").text()).toBe("test-my-text")
     })
