51c9d284d3dd3b7bcf9f9d3da618e2c541677766
Benedikt Kulmann <benedikt@kulmann.biz>
2022-07-19 15:48:25 +0200

    Remove debounce from lazy loading option in OcTableCellData
    Turns out that the debounce option causes significant load during
    rendering, hence it's been removed.

--- src/composables/useIsVisible/index.js
+++ src/composables/useIsVisible/index.js
@@ -1,30 +1,13 @@
 import { onBeforeUnmount, ref, watch } from "@vue/composition-api"
 
 /**
- * once ODS has lodash this debounce implementation can be replaced with the one from lodash.
- * @param delay
- * @param callback
- * @returns {(function(...[*]=): void)|*}
- */
-const debounce = (callback, delay = 0) => {
-  let id = null
-  return (...args) => {
-    window.clearTimeout(id)
-    id = window.setTimeout(() => {
-      callback.apply(null, args)
-    }, delay)
-  }
-}
-
-/**
  *
  * @param {Ref<Element>} target - ref with element to be observed
  * @param {('show'|'showHide')} mode - showHide shows and hides the element on screen enter or leave, show only detects entering the screen and the keeps it rendered
  * @param {string} rootMargin - margin that will be added around the element to detect visibility
- * @param {number} delay - defines the debounce delay of the visibility detection
  * @returns {{isVisible: Ref<boolean>}}
  */
-export const useIsVisible = ({ target, mode = "show", rootMargin = "100px", delay = 0 }) => {
+export const useIsVisible = ({ target, mode = "show", rootMargin = "100px" }) => {
   const isSupported = window && "IntersectionObserver" in window
   if (!isSupported) {
     return {
@@ -34,7 +17,7 @@ export const useIsVisible = ({ target, mode = "show", rootMargin = "100px", dela
 
   const isVisible = ref(false)
   const observer = new IntersectionObserver(
-    debounce(([{ isIntersecting }]) => {
+    ([{ isIntersecting }]) => {
       isVisible.value = isIntersecting
       /**
        * if given mode is `showHide` we need to keep the observation alive.
@@ -50,7 +33,7 @@ export const useIsVisible = ({ target, mode = "show", rootMargin = "100px", dela
       }
 
       observer.unobserve(target.value)
-    }, delay),
+    },
     {
       rootMargin,
     }
