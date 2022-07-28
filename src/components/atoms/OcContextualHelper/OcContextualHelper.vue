bb1202aea7a8409effc1eccd85b39ebc51a6bfde
JanAckermann <jackermann@owncloud.com>
2022-07-28 17:29:19 +0200

    Redesign contextual helper

--- src/components/atoms/OcContextualHelper/OcContextualHelper.vue
+++ src/components/atoms/OcContextualHelper/OcContextualHelper.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="oc-contextual-helper">
     <oc-button :id="buttonId" appearance="raw">
-      <oc-icon name="question" fill-type="line" size="small" />
+      <oc-icon name="question" fill-type="line" size="close" />
     </oc-button>
-    <oc-drop :drop-id="dropId" :toggle="toggleId" mode="click" close-on-click>
+    <oc-drop class="oc-width-1-1" :drop-id="dropId" :toggle="toggleId" mode="click" close-on-click>
       <div class="info-drop-content">
+        <div v-if="title" class="oc-flex oc-flex-between info-title oc-border-b oc-pb-s">
+          <h1 class="oc-m-rm" v-text="title" />
+          <oc-button appearance="raw">
+            <oc-icon name="close" fill-type="line" size="medium" />
+          </oc-button>
+        </div>
         <p class="info-text" v-text="text" />
-        <ul v-if="list.length" class="info-list oc-pl-l">
-          <li v-for="(item, index) in list" :key="index" class="oc-pl-rm">
-            {{ item }}
-          </li>
-        </ul>
+        <dl v-if="list.length" class="info-list">
+          <component
+            :is="item.headline ? 'dt' : 'dd'"
+            v-for="(item, index) in list"
+            :key="index"
+            class="oc-ml-rm"
+            :class="{ headline: item.headline }"
+          >
+            {{ item.text }}
+          </component>
+        </dl>
         <p class="info-text-end" v-text="endText" />
         <oc-button
           v-if="readMoreLink"
@@ -39,9 +51,15 @@ export default {
   status: "unreleased",
   components: { OcButton, OcIcon, OcDrop },
   props: {
+    title: {
+      type: String,
+      required: false,
+      default: "",
+    },
     text: {
       type: String,
       required: false,
+      default: "",
     },
     list: {
       type: Array,
@@ -51,10 +69,12 @@ export default {
     endText: {
       type: String,
       required: false,
+      default: "",
     },
     readMoreLink: {
       type: String,
       required: false,
+      default: "",
     },
   },
   computed: {
@@ -83,6 +103,24 @@ export default {
   }
   .info-more-link {
     font-size: var(--oc-font-size-small) !important;
+  }
+  .info-title {
+    align-items: center;
+
+    h1 {
+      font-size: 1.125rem;
+      font-weight: normal;
+    }
+  }
+  .info-list {
+    .headline {
+      &:first-child {
+        margin-top: 0;
+      }
+      font-weight: bold;
+      margin-bottom: var(--oc-space-xsmall);
+      margin-top: var(--oc-space-small);
+    }
   }
 }
 </style>
@@ -121,11 +159,13 @@ export default {
   computed: {
     helperContent() {
       return {
-        text: this.$gettext("Invite persons or groups to access this file or folder."),
+        title: this.$gettext('Choose how access is granted '),
+        text: this.$gettext("Share a file or folder by link"),
         list: [
-          this.$gettext("Enter a name or group to share this item"),
-          this.$gettext("If you share a folder,  all of its contents and subfolders will be shared with the entered persons or groups"),
-          this.$gettext("Invited persons or groups will be notified via e-mail or in-app notification"),
+          {text: this.$gettext("Only invited people can access"), headline: true},
+          {text: this.$gettext("Only people from the list \"Invited people\" can access. If there is no list, no people are invited yet.")},
+          {text: this.$gettext("Everyone with the link"), headline: true },
+          {text: this.$gettext("Everyone with the link can access. Note: If you share this link with people from the list \"Invited people\", they need to login-in so that their individual assigned permissions can take effect. If they are not logged-in, the permissions of the link take effect.") }
         ],
         endText: this.$gettext("Invited persons can not see who else has access"),
         readMoreLink: "https://owncloud.design"
